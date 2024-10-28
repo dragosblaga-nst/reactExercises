37ef38527872345c35da399d8563e270d4ce2d26
Dragos Blaga <dragos.blaga@ro.neusoft.com>
2024-10-28 15:57:05 +0200

    props exercise - listaimpresii

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Container from "react-bootstrap/Container";
+import Impresii from "./listaimpresii";
+import FormImpresii from "./formimpresii";
 
-function App() {
+export default function App() {
+  const [lista, setLista] = useState([]);
+
+  const adaugInlista = (impresie) => {
+    const nr = lista.length + 1;
+    impresie.id = nr; //  Adaug și câmpul id
+    setLista([...lista, impresie]); //  JS, cursul 3, operatorul spread!
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <h1 style={{ textAlign: "center" }}>Impresii</h1>
+      <Impresii listaImpresii={lista} />
+      <FormImpresii adaimp={adaugInlista} />
+    </Container>
   );
 }
-
-export default App;
